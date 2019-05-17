60dcf45f37bd61443856f473b0727c0585774dec
Jake <Jake@das42.com>
2019-05-17 15:46:35 -0400

    first pass at model predict function

--- Functions/modelPredict.R
+++ Functions/modelPredict.R
@@ -1,5 +1,9 @@
 modelPredict = function(modelName, testData, modelVersion = 1){
   #Get model predictions
+  if(any(is.na(modelVersion))){
+    modelVersion = 1;
+  }
+  tes
   #Add to model environment
   #Return results
 }
