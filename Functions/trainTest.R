7529bb6ffcf5024fee0a72d88561d9ddf42d7b69
Jake <Jake@das42.com>
2019-05-18 16:16:45 -0400

    Adds print out to trainTest function

--- Functions/trainTest.R
+++ Functions/trainTest.R
@@ -1,16 +1,18 @@
-trainTest = function(data, ratio = 0.5){
-  if(any(is.na(ratio))){
-    ratio = 0.5;
+trainTest = function(data, trainingRatio = 0.5){
+  if(any(is.na(trainingRatio))){
+    trainingRatio = 0.5;
   }
   tryCatch(
     {
       dataSet = exec(paste0('dataEnv$', data));
       dataLength = nrow(dataSet);
-      rows = sample(1:dataLength, size = round(ratio * dataLength, 0), replace = FALSE);
+      rows = sample(1:dataLength, size = round(trainingRatio * dataLength, 0), replace = FALSE);
       trainSet = dataSet[rows];
       testSet = dataSet[-rows];
       assign(paste0(data, 'Train'), trainSet, envir = dataEnv);
+      print(paste0("Assigning training set '", paste0(data, 'Train'), "' with ", nrow(dataSet[rows]), " rows."));
       assign(paste0(data, 'Test'), testSet, envir = dataEnv);
+      print(paste0("Assigning test set '", paste0(data, 'Test'), "' with ", nrow(dataSet[-rows]), " rows."));
     },
     warning = function(cond) {
       message(cond);
